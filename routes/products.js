01b423ed96e7736bfd71ba73e4c131d2713af96f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-16 10:35:23 -0600

    CRUD stub routes

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,42 @@ const Router = require('express').Router;
 const router = Router();
 
 //Get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.json({
         message: "Get all products",
+    });
+});
+
+// POST Create a new product
+router.post('/', async (req, res) => {
+    res.json({
+        message: "New product saved."
+    });
+});
+
+// GET a single product by ID
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    res.json({
+        message: "Single Product",
+        productId: id
+       
+    });
+});
+
+// PUT Update a product by ID
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    res.json({
+        message: `Updated ${id} Product Successful.`
+    });
+});
+
+// DELETE a product by ID
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    res.json({
+        message: `Deleted ${id} Product Successful.`
     });
 });
 
