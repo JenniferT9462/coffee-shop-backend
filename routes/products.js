4d96c2a118b655b033aa268c5cddbfee4548e7bf
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-16 20:26:06 -0600

    Get all products and post new product route

--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,47 @@
 const Router = require('express').Router;
 const router = Router();
 
+//Import Product schema
+const Product = require('../models/Product');
+
 //Get all products
 router.get('/', async (req, res) => {
-    res.json({
-        message: "Get all products",
-    });
+    try {
+        const products = await Product.find();
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+    // res.json({
+    //     message: "Get all products",
+    // });
 });
 
 // POST Create a new product
 router.post('/', async (req, res) => {
-    res.json({
-        message: "New product saved."
+    //Extract the request body containing the product data
+    const data = req.body;
+    // Create a new Product instance w/provided data
+    const product = new Product({
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        category: data.category,
+        stock: data.stock,
+        imageUrl: data.imageUrl
     });
+    try {
+        // Save the product data to the database
+        const savedProduct = await product.save();
+        console.log(savedProduct);
+        res.json(savedProduct);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Failed to save product." });
+    }
+    // res.json({
+    //     message: "New product saved."
+    // });
 });
 
 // GET a single product by ID
