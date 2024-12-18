5ae9754eab6a5963e19272e92034115ce941703b
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-17 21:06:44 -0600

    Completed and tested login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
+//Use jwt secret key
+const secretKey = process.env.JWT_SECRET;
+const secretKeyExpires = process.env.JWT_EXPIRES_IN;
+
 //Register a new user
 router.post('/register', async (req, res) => {
     // Extract the request body containing the user data
@@ -28,10 +32,39 @@ router.post('/register', async (req, res) => {
 })
 
 //Login a User
-router.post('/login', (req, res) => {
-    res.json({
-        message: "User is now logged in."
-    })
+router.post('/login', async (req, res) => {
+    // Extract email and password from request body
+    const { email, password } = req.body;
+
+    try {
+        // Step 1: Find the user by email
+        const user = await User.findOne({ email });
+        // If not user send error
+        if (!user) {
+            return res.status(401).json({ error: "Invalid email or password." });
+        } 
+
+        // Step 2: Compare provided password with the hashed password
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+        // Step 3: Generate a JWT token
+        const token = jwt.sign(
+            { id: user._id, email: user.email, role: user.role },
+            // Secret key for signing 
+            secretKey, 
+            // Token expiration
+            { expiresIn: secretKeyExpires }
+        );
+        // Step 4: Send response with the token
+        res.status(200).json({ message: "Login successful", token })
+    } catch (error) {
+        // Log the error for debugging
+        console.error(error);
+        // Return a server error
+        res.status(500).json({ error: "Server error" });
+    }
 })
 
 //Export routes
