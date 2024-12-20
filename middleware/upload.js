c9b60c824b845b09f5eaa5bf8edbd7fa7b40e72c
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-19 20:24:28 -0600

    middleware for handling file uploads

---
+++ middleware/upload.js
@@ -0,0 +1,39 @@
+const multer = require('multer');
+const path = require('path');
+
+// Set Storage Engine
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+        );
+    },
+});
+
+//Initialize Upload
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10000000},
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    },
+}).single('image');
+
+// Check File Type
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
+module.exports = upload;
