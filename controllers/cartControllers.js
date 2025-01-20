a8eb38524567cde9417beaffa8b6c6752a86a712
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-20 13:20:04 -0600

    fixing cart routes

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -3,7 +3,8 @@
 const Cart = require('../models/Cart');
 
 async function getOrCreateCart(req, res, next) {
-    const userId = req.body.userId; // Fetch userId from request body
+    // const userId = req.body.userId; // Fetch userId from request body
+    const userId = req.userId
     if (!userId) {
       return res.status(400).json({ error: "userId is required" });
     }
